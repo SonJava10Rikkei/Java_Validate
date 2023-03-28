19834139482174358ed77fa31670de0068b08f9c
SonJava10Rikkei <sonjapan7@gmail.com>
2023-03-28 08:26:38 +0700

    Java_Validate

---
+++ src/src/config/Config.java
@@ -0,0 +1,9 @@
+package src.config;
+
+import java.util.Scanner;
+
+public class Config {
+    public static Scanner getScanner(){
+        return new Scanner(System.in);
+    }
+}
---
+++ src/src/view/View.java
@@ -0,0 +1,16 @@
+package src.view;
+
+import src.service.Service;
+
+public class View {
+    public static void showMenu(){
+        System.out.println("nhập lựa chọn của bạn \n" +
+                "1. hiển thị danh sách \n" +
+                "2. thêm sản phẩm \n" +
+                "3. xóa sản phẩm \n" +
+                "4. sửa sản phẩm ");
+    }
+    public void showListProduct(){
+        System.out.println(Service.showListProduct());
+    }
+}
